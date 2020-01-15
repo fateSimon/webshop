d81811631d312607137add934ad481cf69b70225
Simon <simon.schaller@lernende.bfo-vs.ch>
2020-01-15 13:35:01 +0100

    Added category

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -1,11 +1,18 @@
 @include('partials.errors')
 
+
 <form action="{{route('product.store')}}" method="POST">
     @csrf
 
     <label for ="name">@lang ('product.name')</label> <input type="text" name="name" value="">
+    <br>
     <label for ="price">@lang ('product.price') </label> <input type="text" name="price" value="">
+    <br>
     <label for ="text">@lang ('product.description')</label> <input type="text" name="description" value="">
+    <br>
+    <select name="topX">
+        <option>Uhren</option>
+    </select>
 
     <input type="submit" value="Produkt erstellen">
 </form>
